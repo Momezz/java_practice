c3a005ccd1db510a4f513e2c195ca97dc20f2739
Momezz <julgomez14@gmail.com>
2023-09-02 20:00:17 -0500

    I create the class charmander and bulbasaur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import com.sun.source.doctree.ValueTree;
+
 public class Main {
   public static void main(String[] args) {
-    String saludo = "Hello Wrld";
-    System.out.println(saludo);
+    Charmander charmanderUno = new Charmander(0.6, 0.5,  "macho", "Mar llamas");
+    Bulbasaur bulbasaurUno = new Bulbasaur();
+
+    bulbasaurUno.setAltura(0.7);
+    bulbasaurUno.setPeso(6.9);
+    bulbasaurUno.setSexo("Macho");
+    bulbasaurUno.setHabilidad("Espesura");
+    bulbasaurUno.setDebilidades();
+
+    charmanderUno.ataqueMarLlamas(bulbasaurUno, 23);
+
+    System.out.println("Los puntos de vida restantes son: ");
+    System.out.println(bulbasaurUno.getPuntosVida());
   }
 }
